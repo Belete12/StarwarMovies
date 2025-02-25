16e35bcdfead6e4cb30856fb70907c022894b544
Samrawit Tsegay <samrawitekubay@gmail.com>
2025-02-25 16:37:01 -0500

    displaying the Data in to html

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 
 // https://swapi.dev/api/people/'
 
+// fetch data from star war  API 
+// store the data in HTML page that displays the list of datas
+
    async function getStarWars() {
     try {     
       const response = await fetch('https://swapi.dev/api/films/');
@@ -13,7 +16,7 @@
 
         var table = document.createElement('thead');
 
-        table.innerHTML = `<tr><td>${movie.title} </td> </tr>`
+        table.innerHTML = `<tr><td>${movie.title} </td><td>${movie.episode_id} </td><td>${movie.director} </td>  <td>${movie.producer} </td><td>${movie.release_date} </td></tr>`
        document.getElementById('ListItem').appendChild(table);
 
       });
@@ -23,4 +26,5 @@
     }
   }
 
+// call the function
   getStarWars();
