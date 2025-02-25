4cf3f59871a589dce10f1a1440cb78d631a3778a
Samrawit Tsegay <samrawitekubay@gmail.com>
2025-02-25 12:37:48 -0500

    testing html

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
-fetch('https://swapi.dev/api/people/')
-    .then(response => response.json())
-    .then(data => console.log(data))
-   .catch(error => console.error('error:', error));
 
+// https://swapi.dev/api/people/'
+
+   async function getStarWars() {
+    try {     
+      const response = await fetch('https://swapi.dev/api/films/');
+      const data = await response.json();
+      const movies = data.results;
+    } catch (error) {
+      console.error(error);
+    }
+  }
