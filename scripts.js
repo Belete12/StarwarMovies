288811c4312cae19e2f7feb42b231b6aeb679eac
Samrawit Tsegay <samrawitekubay@gmail.com>
2025-02-25 12:54:33 -0500

    Adding the list of items in the html

--- scripts.js
+++ scripts.js
@@ -5,8 +5,22 @@
     try {     
       const response = await fetch('https://swapi.dev/api/films/');
       const data = await response.json();
+
       const movies = data.results;
+
+
+      movies.forEach(movie => {
+
+        var table = document.createElement('thead');
+
+        table.innerHTML = `<tr><td>${movie.title} </td> </tr>`
+       document.getElementById('ListItem').appendChild(table);
+
+      });
+   
     } catch (error) {
       console.error(error);
     }
   }
+
+  getStarWars();
