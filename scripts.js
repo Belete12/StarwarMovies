c5d1ed133cdfea0881edf47faa3d6076770b5e94
Samrawit Tsegay <samrawitekubay@gmail.com>
2025-02-25 22:29:17 -0500

    Adding two apis

--- scripts.js
+++ scripts.js
@@ -1,30 +1,36 @@
-
 // https://swapi.dev/api/people/'
-
 // fetch data from star war  API 
 // store the data in HTML page that displays the list of datas
 
-   async function getStarWars() {
-    try {     
-      const response = await fetch('https://swapi.dev/api/films/');
-      const data = await response.json();
+  const films1= 'https://swapi.dev/api/films/';
+  const character2='https://swapi.dev/api/people/';
 
-      const movies = data.results;
+  
+async function getStarWars() {
+    try{
+    const[film_res, char_res] = await Promise.all([
+        fetch(films1),
+        fetch(character2)
+    ]);
+    const films_data= await film_res.json();
+    const char_data=await char_res.json();
+    const movies=films_data.results;
+    const characters= char_data.results;
+    movies.forEach(movie => {
+        characters.forEach(character =>{
+            var table = document.createElement('thead');
+            table.innerHTML = `<tr><td>${movie.title} </td><td>${movie.episode_id} </td><td>${movie.director} </td>  <td>${movie.producer} </td><td>${character.name} </td></tr>`
+        document.getElementById('ListItem').appendChild(table);
 
+        })
 
-      movies.forEach(movie => {
-
-        var table = document.createElement('thead');
-
-        table.innerHTML = `<tr><td>${movie.title} </td><td>${movie.episode_id} </td><td>${movie.director} </td>  <td>${movie.producer} </td><td>${movie.release_date} </td></tr>`
-       document.getElementById('ListItem').appendChild(table);
-
-      });
-   
-    } catch (error) {
-      console.error(error);
+        });
+    }// end try
+    catch(error){
+        console.log(error)
     }
-  }
+} // end function
 
-// call the function
+
+
   getStarWars();
